c4444f07ded7678266025124b764ea198e960d0a
Solaris <106869539+solaris57@users.noreply.github.com>
2023-05-24 15:57:01 +0200

    Update main.cs
    Used a switch case instead of an if-else statement.

--- source/main.cs
+++ source/main.cs
@@ -1,4 +1,3 @@
-// Have fun :) - Solaris
 using System;
 using System.IO.Ports;
 using System.Text;
@@ -9,7 +8,6 @@ namespace UltrashipU2
     {
         static void Main(string[] args)
         {
-
             // Configure serial port
             SerialPort port = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
             port.ReadTimeout = 1000;
@@ -29,36 +27,35 @@ namespace UltrashipU2
                     Console.WriteLine("Waiting...");
                     continue;
                 }
-                //Decoding
+                
+                // Decoding
                 string response = BitConverter.ToString(buffer, 0, bytesRead);
 
-                if (response == "02")
+                switch (response)
                 {
-                    Console.WriteLine("---SEND button pressed---");
-                }
-                else if (response.StartsWith("0B-44"))
-                {
-                    double Weigh;
-                    if (double.TryParse(Encoding.ASCII.GetString(buffer, 2, bytesRead - 2), out Weigh))
-                    {
-                        Console.WriteLine("Peso: {0}", Weigh);
-                        //Note: You could add the measure unit of your choice here.
-                    }
-                    else
-                    {
-                        Console.WriteLine("Decode error");
-                    }
-                }
-                else if (response.StartsWith("47-4B-03"))
-                {
-                    Console.WriteLine("---End of data transmission---");
-                }
-                else
-                {
-                    Console.WriteLine("Unknown response: {0}", response);
+                    case "02":
+                        Console.WriteLine("---SEND button pressed---");
+                        break;
+                    case string r when r.StartsWith("0B-44"):
+                        double weight;
+                        if (double.TryParse(Encoding.ASCII.GetString(buffer, 2, bytesRead - 2), out weight))
+                        {
+                            Console.WriteLine("Weight: {0}", weight);
+                            // Note: You could add the measure unit of your choice here.
+                        }
+                        else
+                        {
+                            Console.WriteLine("Decode error");
+                        }
+                        break;
+                    case "47-4B-03": //Update this vaule! My scale was set to g and Kg
+                        Console.WriteLine("---End of data transmission---");
+                        break;
+                    default:
+                        Console.WriteLine("Unknown response: {0}", response);
+                        break;
                 }
             }
-
         }
     }
 }
